b1e78fa2c09c1271a890a2e941c64b707681346d
Josie <josipakukor@gmail.com>
2024-02-06 17:27:09 +0100

    adding lesson 11 lesson practice files

---
+++ scripts/11-todo-list.js
@@ -0,0 +1,26 @@
+const todoList = ["wash hair", "wash dishes"];
+
+renderTodoLost();
+
+function renderTodoLost() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+  }
+  console.log(todoListHTML);
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
+}
+function addTodo(todo) {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+  todoList.push(name);
+  console.log(todoList);
+
+  inputElement.value = "";
+
+  renderTodoLost();
+}
